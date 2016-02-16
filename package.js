d2d8b072cf00f280781ba9927eb8f30916d2a4b1
Hamish Rickerby <hamish@happtic.com>
2016-02-16 14:42:53 +1100

    Repoint to my repo, drop comments

--- package.js
+++ package.js
@@ -1,15 +1,12 @@
 Package.describe({
-    name: 'ajduke:bootstrap-tagsinput',
+    name: 'rickerbh:bootstrap-tagsinput',
     summary: 'jQuery plugin providing a Twitter Bootstrap user interface for managing tags, repackaged for Meteor',
     version: '0.7.0',
-    git: "https://github.com/ajduke/meteor-bootstrap-tagsinput.git"
+    git: "https://github.com/rickerbh/meteor-bootstrap-tagsinput.git"
 });
 
 Package.on_use(function (api) {
-    // dependecies required by package
     api.use('jquery@1.0.1', 'client');
-    // adding the required file for package
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', 'client');
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', 'client');
-   // api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput-angular.js', 'client');
 });
